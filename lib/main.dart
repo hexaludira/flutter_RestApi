9d65a4e0cd4f182720a687dc658b604af2ac5fbe
hexaludira <hexaludira@gmail.com>
2021-01-17 12:13:46 +0700

    Coba material search bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_1/models/profile.dart';
 import 'package:flutter_1/screens/form_add_screen.dart';
 import 'package:flutter_1/screens/login_view.dart';
 import 'package:flutter_1/screens/register_view.dart';
 import 'package:flutter_1/utils/api.dart';
 import 'package:flutter_1/screens/splash_screen.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+
 
 void main() {
   runApp(MaterialApp(
@@ -18,9 +23,9 @@ void main() {
     //initialRoute: "/",
     routes: <String, WidgetBuilder>{
       '/' : (BuildContext context) => new SplashScreenPage(),//LoginPage(),
-      '/hal_satu': (BuildContext context) => new Pandora(),
-      '/hal_dua': (BuildContext context) => new Pandora2(),
-      '/hal_tiga': (BuildContext context) => new PandoraList(),
+      // '/hal_satu': (BuildContext context) => new Pandora(),
+      // '/hal_dua': (BuildContext context) => new Pandora2(),
+      // '/hal_tiga': (BuildContext context) => new PandoraList(),
       RegisterPage.routeName : (context) => RegisterPage(),
     },
   ));
@@ -34,6 +39,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   BuildContext context;
   ApiService apiService;
+  TextEditingController _filter = TextEditingController();
+  List<Profile> profiles = List();
+  List<Profile> filteredProfiles = List();
+  Profile profile = Profile();
+  bool cari_aktif;
+  
 
   @override
   void initState() {
@@ -46,22 +57,22 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     this.context = context;
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.white),
-        title: Text(
-          "Metal Problem",
-          style: TextStyle(color: Colors.white),
-        ),
-        // actions: <Widget>[
-        //   IconButton(
-        //     icon: const Icon(Icons.add),
-        //     tooltip: 'Tambah Data',
-        //     onPressed: () {
-        //       Navigator.push(context, MaterialPageRoute(builder: (context) => FormAddScreen()),);
-        //     }
-        //   ),
-        // ],
-      ),
+      // appBar: AppBar(
+      //   iconTheme: IconThemeData(color: Colors.white),
+      //   title: Text(
+      //     "Metal Problem",
+      //     style: TextStyle(color: Colors.white),
+      //   ),
+      //   // actions: <Widget>[
+      //   //   IconButton(
+      //   //     icon: const Icon(Icons.add),
+      //   //     tooltip: 'Tambah Data',
+      //   //     onPressed: () {
+      //   //       Navigator.push(context, MaterialPageRoute(builder: (context) => FormAddScreen()),);
+      //   //     }
+      //   //   ),
+      //   // ],
+      // ),
       body: SafeArea(
         child: FutureBuilder(
           future: apiService.getProfiles(),
@@ -71,8 +82,21 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Text("Something wrong with message: ${snapshot.error.toString()}"),
             );
           } else if (snapshot.connectionState == ConnectionState.done) {
+
             List<Profile> profiles = snapshot.data;
-            return _buildListView(profiles);
+            return FloatingSearchAppBar(
+              title: Text('Metal Problem', style: TextStyle(color: Colors.white),),
+              iconColor: Colors.white,
+              transitionDuration: const Duration(milliseconds: 800),
+              colorOnScroll: Colors.grey[600],
+              liftOnScrollElevation: 10.0,
+              color: Colors.grey[600],
+              hint: 'Cari...',
+              hintStyle: TextStyle(color: Colors.white54),
+              onQueryChanged: (query) => onSearch(query, profiles),
+            
+              
+            );
           } else {
             return Center(
               child: CircularProgressIndicator(),
@@ -193,10 +217,129 @@ class _HomeScreenState extends State<HomeScreen> {
           itemCount: profiles.length,
         ),
       ),
-    );
+      );
+    
      
   }
 
+  Widget onSearch(String textFilter, List<Profile> listCari) {
+    List<Profile> tempList = List();
+    if(textFilter.isEmpty){
+      setState(() {     
+      });
+    } else {
+      listCari.forEach((element) { 
+        if (element.location.toLowerCase().contains(textFilter)){
+          //print(element);
+           tempList.add(element);
+        }
+      });
+        filteredProfiles = tempList;
+      
+      //profiles = tempList;
+      // profiles.forEach((element) {
+      //   print(element);
+      // });
+      //return profiles;
+    }
+
+    return ListView.builder(
+          itemBuilder: (context, index) {
+            Profile profile = filteredProfiles[index];
+            return Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                          profile.location + " \u25BA " + profile.detail,
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                      //Text(profile.detail, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.grey[700]),),
+                      Text(profile.date, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.grey[700]),),
+                      Text('Status: ' + profile.status),
+                      Text('Ket: ' + profile.remark),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          FlatButton(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text("Warning"),
+                                    content: Text("Yakinkah kau menghapus data ${profile.id}?"),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                        child: Text("Pastinya"),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                          //Navigator.push(context, MaterialPageRoute(builder: (context) => FormAddScreen()),);
+                                          apiService.deleteData(profile.id).then((isSuccess) {
+                                            if(isSuccess) {
+                                              setState(() {});
+                                            } else {
+                                              return AlertDialog(title: Text("Gagal"), content: Text("gagal terus"),);  
+                                            }
+
+                                          });
+                                          
+                                          
+                                          // apiService.deleteProfile(profile.id).then((isSuccess) {
+                                          //   if(isSuccess) {
+                                          //     setState(() {});
+                                              
+                                          //     Scaffold.of(context).showSnackBar(SnackBar(content: Text("Hapus data berhasil")));
+                                          //   } else {
+                                          //     Scaffold.of(context).showSnackBar(SnackBar(content: Text("Yahh.. Hapus data gagal:(")));
+                                          //   }
+                                          // });
+                                        }, 
+                                        
+                                      ),
+                                      FlatButton(
+                                        child: Text("Ga jadi"),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                }
+                              );
+                            }, 
+                            child: Text(
+                              "Delete",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                          FlatButton(
+                            onPressed: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => FormAddScreen(profile: profile,),));
+                              
+                            }, 
+                            child: Text(
+                              "Edit",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+          itemCount: profiles.length,
+        );
+
+  }
+  
   Future refreshData() async{
     await Future.delayed(Duration(seconds: 2));
     apiService.getProfiles();
@@ -205,107 +348,5 @@ class _HomeScreenState extends State<HomeScreen> {
   
 }
 
-// class ApiTes extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     ApiService().getProfiles().then((value) => print("value: $value"));
-//     return Container(
-      
-//     );
-//   }
-// }
 
- 
-class Pandora2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-        title: Text(
-          "Halaman Dua",
-          style: TextStyle(color: Colors.grey),
-        ),
-        backgroundColor: Colors.lime,
-        centerTitle: true,
-      ),
-      body: Center(
-        child : Container(
-          child: new Text("data"),
-        )
-      ),
-    );
-  }
-}
 
-//halaman awal
-class Pandora extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: new AppBar(
-          leading: IconButton(icon: Icon(Icons.translate), onPressed: null),
-         actions: <Widget>[IconButton(icon: Icon(Icons.tag_faces), onPressed: null)],
-          title: Text(
-            "Pandora List",
-            style: TextStyle(color: Colors.grey[900]),
-          ),
-          backgroundColor: Colors.blue,
-          centerTitle: true,
-        ),
-        body: Container(
-          child: ListView(
-            
-          ),
-        ),
-        // body: Center(
-        //   child: RaisedButton(
-            
-        //     child: Text("Selanjutnya", style: TextStyle(fontSize: 30)),
-        //     onPressed: (){
-        //       Navigator.pushNamed(context, '/hal_dua');
-        //     },
-        //   ),
-        // )
-        );
-  }
-}
-
-class PandoraList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return ListView(
-     children: <Widget>[
-       ListTile(
-         title : Text("Final Fantasy VII Remake"),
-         leading: Icon(Icons.games),
-       ),
-       ListTile(
-         title : Text("Resident Evil 7"),
-         leading: Icon(Icons.gradient),
-       ),
-     ],
-      
-    );
-  }
-}
-
-// class ListView extends
-
-//     // return MaterialApp(
-//     //   title: 'Sego Kucing',
-//     //   home: Scaffold(
-//     //     backgroundColor: Colors.lightGreenAccent,
-//     //     appBar: AppBar(
-//     //       title: Text('Sego Kucing pak No'),
-//     //       backgroundColor: Colors.blueGrey,
-//     //     ),
-//     //     body: Column(
-//     //       children:<Widget>[
-//     //         Image.network('https://www.petanikode.com')
-//     //       ],
-//     //       ),
-//     //   ),
-//     // );
-//   }
-
-// }
